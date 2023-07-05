6959e40692a5b1fde4a34c64a64b27d7a99cd376
dohan0511 <63398428+dohan0511@users.noreply.github.com>
2023-07-05 14:17:26 +0000

    end of work at Jul 5th, changed existing structures to flexible(div)

--- readyMenu.js
+++ readyMenu.js
@@ -1,5 +1,5 @@
-function showStats(pokemon) {
-  var statText = document.getElementById("infoText");
+function showStats(pokemon, text) {
+  var statText = document.getElementById("upperText");
   var typeText = " ";
   pokemon.types.forEach(function(type) {
     typeText += ("[" + type + "] ");
@@ -8,8 +8,8 @@ function showStats(pokemon) {
     "<br>특수공격 | " + pokemon.sp_atk + "<br>특수방어 | " + pokemon.sp_def + "<br>스피드 | " + pokemon.spd;
 }
 
-function showMoves(pokemon) {
-  var moveText = document.getElementById("infoText");
+function showMoves(pokemon, text) {
+  var moveText = document.getElementById("upperText");
   moveText.innerHTML = pokemon.name + "  Lv. " + pokemon.lvl + "<br>스킬 1 | " + pokemon.skl1.name + "<br>스킬 2 | " + pokemon.skl2.name + "<br>스킬 3 | " + pokemon.skl3.name +
     "<br>스킬 4 | " + pokemon.skl4.name;
 }
--- script.js
+++ script.js
@@ -7,17 +7,29 @@ var myPokemon = new Pokemon("리자몽", 100, "째려보기", "베어가르기",
 window.onload = function() {
   showStats(myPokemon);
   var status = 0
-  var button = document.getElementById("button1");
-  button.addEventListener("click", function() {
+  var upperText = document.getElementById("upperText");
+  var lowerText = document.getElementById("lowerText");
+  var button1 = document.getElementById("button1");
+  var button2 = document.getElementById("button2");
+  var startBattleButton = document.createElement('button');
+  var changeMenuButton = document.createElement('button');
+  changeMenuButton.innerText = '기술 메뉴로 이동하기';
+  startBattleButton.innerText = '배틀 시작';
+  changeMenuButton.addEventListener("click", function() {
     if (status == 0) {
-      button.textContent = '능력치 메뉴로 이동하기';
+      changeMenuButton.innerText = '능력치 메뉴로 이동하기';
       showMoves(myPokemon);
       status = 1;
     }
     else {
-      button.textContent = '기술 메뉴로 이동하기';
+      changeMenuButton.innerText = '기술 메뉴로 이동하기';
       showStats(myPokemon);
       status = 0;
     }
   });
+  startBattleButton.addEventListener('click', function() {
+    
+  })
+  button1.appendChild(changeMenuButton);
+  button2.appendChild(startBattleButton);
 };
